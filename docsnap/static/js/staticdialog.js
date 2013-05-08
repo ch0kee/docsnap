397e69fa8649c3348caaf80b1036e0d52913ec52
ch0kee <ch0kee@hotmail.hu>
2013-05-08 02:36:11 +0200

    - removed accidentally added replicate folder

--- docsnap/static/js/staticdialog.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  DocSnap.showDialog(__dlgContent.replace('\\n','\n'),
-                     [{text: __dlgButton , click: function() {window.location.href = __dlgTarget;} }]);
-});
-
-
-
-
-
